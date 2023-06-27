659583fe241194867614797d739983595661a75f
Adrian <adrianvalverde45@gmail.com>
2023-06-26 21:24:52 -0400

    commit tree edd5446abf0c114ca8696aea603a56cbfd4d89cb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,38 @@
 require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    array_of_sentences = self.split(" ")
+    total_sentences = []
+    
+    array_of_sentences.map do |sentence|
+
+      if sentence.empty?
+        nil
+      elsif sentence.sentence?
+        total_sentences << sentence
+      elsif sentence.question?
+        total_sentences << sentence
+      elsif sentence.exclamation?
+        total_sentences << sentence
+      end
+    end
+
+    total_sentences.count
 
   end
+
+      
 end
